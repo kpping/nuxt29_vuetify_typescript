9af1ab8ab0a0afacee2ef5797935dc905e8473dd
Krittanan Pingclasai <kp@kpping.me>
2020-02-26 12:08:08 +0700

    use @nuxtjs/eslint-config-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,28 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    warnOnUnsupportedTypeScriptVersion: false
+    warnOnUnsupportedTypeScriptVersion: false,
   },
   extends: [
-    '@nuxtjs',
+    '@nuxtjs/eslint-config-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:nuxt/recommended',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
   ],
-  plugins: [
-    '@typescript-eslint',
-    'prettier'
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ["error", { "argsIgnorePattern": "^_" }],
-    'prettier/prettier': "warn"
-  }
-}
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
+    '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': 'warn',
+  },
+};
